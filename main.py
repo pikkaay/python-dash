a2d59ae6bef437f972bd2f94bb591d556ad2b628
midhun <midhun.pk@sar-group.com>
2018-12-03 19:15:34 +0530

    test

--- main.py
+++ main.py
@@ -6,16 +6,23 @@ import plotly as py
 import random
 import plotly.graph_objs as go
 from collections import deque
+import datetime
 
 print(dash.__version__)
 print(py.__version__)
 # py.offline.init_notebook_mode(connected=True)
 
 
-X = deque(maxlen=20)
-X.append(1)
+start = datetime.datetime.now() - datetime.timedelta(seconds=30) + datetime.timedelta(hours=5, minutes = 30)
+X = deque(maxlen=600)
+X.append(start)
+for i in range(600):
+    X.append(X[-1]+datetime.timedelta(seconds=1))
+
 Y = deque(maxlen=20)
 Y.append(1)
+for i in range(20):
+    Y.append(Y[-1]+1)
 
 
 app = dash.Dash(__name__)
@@ -24,7 +31,7 @@ app.layout = html.Div(
         dcc.Graph(id='live-graph', animate=True),
         dcc.Interval(
             id='graph-update',
-            interval=1*500
+            interval=2000
         ),
     ]
 )
@@ -32,8 +39,10 @@ app.layout = html.Div(
 @app.callback(Output('live-graph', 'figure'),
               events=[Event('graph-update', 'interval')])
 def update_graph_scatter():
-    X.append(X[-1]+1)
-    Y.append(Y[-1]+Y[-1]*random.uniform(-0.1,0.1))
+        
+
+    X.append(X[-1]+datetime.timedelta(seconds=1))
+    Y.append(Y[-1]+1)
 
     data = py.graph_objs.Scatter(
             x=list(X),
@@ -42,10 +51,9 @@ def update_graph_scatter():
             mode= 'lines+markers'
             )
 
-    return {'data': [data],'layout' : py.graph_objs.Layout(xaxis=dict(range=[min(X),max(X)+5]),
+    return {'data': [data],'layout' : py.graph_objs.Layout(
+        # xaxis=dict(range=[min(X),max(X)+5]),
                                                 yaxis=dict(range=[min(Y),max(Y)]),)}
-
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
